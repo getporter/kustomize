977087ea604e43cc1c714cad10908ae87591d0b5
Donald Stewart <don.stewart@icloud.com>
2019-09-27 23:57:09 +0100

    .

--- vendor/github.com/deislabs/porter/pkg/context/context.go
+++ vendor/github.com/deislabs/porter/pkg/context/context.go
@@ -57,9 +57,7 @@ func (cw *CensoredWriter) SetSensitiveValues(vals []string) {
 func (cw *CensoredWriter) Write(b []byte) (int, error) {
 	auditedBytes := b
 	for _, val := range cw.sensitiveValues {
-		if strings.TrimSpace(val) != "" {
-			auditedBytes = bytes.Replace(auditedBytes, []byte(val), []byte("*******"), -1)
-		}
+		auditedBytes = bytes.Replace(auditedBytes, []byte(val), []byte("*******"), -1)
 	}
 
 	_, err := cw.writer.Write(auditedBytes)
