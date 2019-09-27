0247fc46a0d16337a58d81e188fe6b7485d08ce3
Donald Stewart <don.stewart@icloud.com>
2019-09-27 01:36:32 +0100

    .

--- pkg/kustomize/version.go
+++ pkg/kustomize/version.go
@@ -1,11 +1,19 @@
 package kustomize
 
 import (
-	"fmt"
-
+	"github.com/deislabs/porter/pkg/mixin"
+	"github.com/deislabs/porter/pkg/porter/version"
 	"github.com/donmstewart/porter-kustomize/pkg"
 )
 
-func (m *Mixin) PrintVersion() {
-	fmt.Fprintf(m.Out, "kustomize mixin %s (%s)\n", pkg.Version, pkg.Commit)
+func (m *Mixin) PrintVersion(opts version.Options) error {
+	metadata := mixin.Metadata{
+		Name: "kustomize",
+		VersionInfo: mixin.VersionInfo{
+			Version: pkg.Version,
+			Commit:  pkg.Commit,
+			Author:  "donmstewart",
+		},
+	}
+	return version.PrintVersion(m.Context, opts, metadata)
 }
--- pkg/kustomize/version_test.go
+++ pkg/kustomize/version_test.go
@@ -4,6 +4,11 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/deislabs/porter/pkg/printer"
+
+	"github.com/deislabs/porter/pkg/porter/version"
+	"github.com/stretchr/testify/require"
+
 	"github.com/donmstewart/porter-kustomize/pkg"
 )
 
@@ -12,10 +17,39 @@ func TestPrintVersion(t *testing.T) {
 	pkg.Version = "v1.2.3"
 
 	m := NewTestMixin(t)
-	m.PrintVersion()
+
+	opts := version.Options{}
+	err := opts.Validate()
+	require.NoError(t, err)
+	m.PrintVersion(opts)
 
 	gotOutput := m.TestContext.GetOutput()
-	wantOutput := "kustomize mixin v1.2.3 (abc123)"
+	wantOutput := "kustomize v0.1 (alpha) by donmstewart"
+	if !strings.Contains(gotOutput, wantOutput) {
+		t.Fatalf("invalid output:\nWANT:\t%q\nGOT:\t%q\n", wantOutput, gotOutput)
+	}
+}
+
+func TestPrintJsonVersion(t *testing.T) {
+	pkg.Commit = "alpha"
+	pkg.Version = "v0.1"
+
+	m := NewTestMixin(t)
+
+	opts := version.Options{}
+	opts.RawFormat = string(printer.FormatJson)
+	err := opts.Validate()
+	require.NoError(t, err)
+	m.PrintVersion(opts)
+
+	gotOutput := m.TestContext.GetOutput()
+	wantOutput := `{
+	  "name": "kustomize",
+	  "version": "v0.1",
+	  "commit": "alpha",
+	  "author": "donmstewart"
+	}`
+
 	if !strings.Contains(gotOutput, wantOutput) {
 		t.Fatalf("invalid output:\nWANT:\t%q\nGOT:\t%q\n", wantOutput, gotOutput)
 	}
